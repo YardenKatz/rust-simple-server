6c4d06addad39676aa1f21158f7e12e0c91fa107
Yarden Katz <yrdnkz@gmail.com>
2023-03-20 13:46:15 +0200

    added threadpool functionality, missing execute

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use std::{
 	fs,
 	io::{prelude::*, BufReader},
-	net::{TcpListener, TcpStream},
+	net::{TcpListener, TcpStream}, thread, time::Duration,
 };
 
+use simple_server::ThreadPool;
+
 fn main() {
-	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+	let listener = TcpListener::bind("127.0.0.1:7879").unwrap();
+	let pool = ThreadPool::new(4);
 
 	for stream in listener.incoming() {
 		let stream = stream.unwrap(); // we ignore errors for simplicity sake. real world situation would handle errors here
 
-		handle_connection(stream);
+		pool.execute(|| {
+			handle_connection(stream);
+		});
 	}
 }
 
@@ -19,10 +24,13 @@ fn handle_connection(mut stream: TcpStream) {
 
 	let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-	let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-		("HTTP/1.1 200 OK", "hello.html")
-	} else {
-		("HTTP/1.1 404 NOT FOUND", "404.html")
+	let (status_line, filename) = match &request_line[..] {
+		"GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+		"GET /sleep HTTP/1.1" => {
+			thread::sleep(Duration::from_secs(5));
+			("HTTP/1.1 200 OK", "hello.html")
+		}
+		_ => ("HTTP/1.1 404 NOT FOUND", "404.html")
 	};
 
 	let contents = fs::read_to_string(filename).unwrap(); // again...
